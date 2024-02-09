493a67fc66f6db77a5d7e224532dab594df09338
Astspace <asteroid.space@gmail.com>
2024-02-09 16:47:45 +0300

    history

--- history.py
+++ history.py
@@ -1,8 +1,10 @@
+import json
 from typing import Protocol
 from weather_api_service import Weather
 from pathlib import Path
 from datetime import datetime
 from weather_formatter import format_weather
+from typing import TypedDict
 
 
 class WeatherStorage(Protocol):
@@ -23,9 +25,31 @@ class TxtFileWeatherStorage:
 def save_weather(weather: Weather, storage: WeatherStorage) -> None:
     storage.save(weather)
 
+class HistoryRecord(TypedDict):
+    date: str
+    weather: str
+
+
 class JSONFileWeatherStorage:
     def __init__(self, jsonfile: Path):
         self._jsonfile = jsonfile
+        self._init_storage()
 
     def save(self, weather: Weather) -> None:
+        history = self._load_history()
+        history.append({"date": str(datetime.now()),
+                        "weather": format_weather(weather)
+                        })
+        self._write(history)
+
+    def _load_history(self) -> list[HistoryRecord]:
         with open(self._jsonfile, "r") as f:
+            return json.load(f)
+
+    def _write(self, history: list[HistoryRecord]) -> None:
+        with open(self._jsonfile, "w") as f:
+            json.dump(history, f, ensure_ascii=False, indent=4)
+
+    def _init_storage(self) -> None:
+        if not self._jsonfile.exists():
+            self._jsonfile.write_text("[]")
