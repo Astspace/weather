51d2991e32d88c5f5a52868a940cc2b06b101ad3
Astspace <asteroid.space@gmail.com>
2024-02-18 22:58:58 +0300

    GUI_1

--- gui.py
+++ gui.py
@@ -2,7 +2,8 @@ import sys
 from weather import main as go_weather
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QGridLayout, QMainWindow
+from PyQt6.QtGui import QPixmap
 
 
 class MainWindow(QWidget):
@@ -13,25 +14,36 @@ class MainWindow(QWidget):
         self.labels()
         self.buttons()
         self.layouts()
+        self.load_images('zastavka.jpg')
         self.setLayout(self.layout)
 
     def window_settings(self):
-        self.setGeometry(700, 300, 350, 250)
+        self.setGeometry(400, 100, 1250, 850)
         self.setWindowTitle("Узнаем погодку")
 
     def labels(self):
-        self.label = QLabel("Погодка:")
+        self.label = QLabel("Хочешь узнать погодку?")
         self.label.setFont(QFont('Arial', 10))
         self.weather = QLabel(go_weather())
 
     def buttons(self):
-        self.button = QPushButton("GO")
+        self.button = QPushButton("Хочу!")
+        self.button.clicked.connect(self.button_clicked)
 
     def layouts(self):
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.layout.addWidget(self.weather, alignment=Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.weather, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    def button_clicked(self):
+        self.layout.addWidget(self.weather, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    def load_images(self, file_name):
+        self.background = QPixmap(file_name)
+        self.label_back = QLabel(self)
+        self.label_back.setPixmap(self.background)
+        self.label_back.resize(self.background.width(), self.background.height())
 
 
 def main():
