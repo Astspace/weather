b66883ff83b3f56ca40d24b5d881717c08593e58
Astspace <asteroid.space@gmail.com>
2024-02-21 16:55:47 +0300

    GUI_6

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from weather import main as go_weather
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QWidget
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import Qt
 
 
 class MainWindow(QWidget):
@@ -20,47 +20,54 @@ class MainWindow(QWidget):
         self.setWindowTitle("Узнаем погодку")
 
     def _labels(self) -> None:
-        self.label = QLabel("Хочешь узнать погодку?", self)
-        self.label.setFont(QFont('Arial', 30))
-        self.label.move(10, 500)
+        self.label = QLabel("<font color=yellow>Хочешь узнать погодку?</font>", self)
+        self.label.setFont(QFont('Cambria', 20))
+        self.label.move(500, 20)
 
     def _buttons(self) -> None:
         self.button = QPushButton("Мяу!", self)
-        self.button.move(100, 200)
+        self.button.move(600, 200)
         self.button.clicked.connect(self._button_clicked)
 
     def _button_clicked(self) -> None:
         if self.button_clicked_count == 0:
-            self.label_weather = QLabel(go_weather(), self)
-            self.label_weather.setFont(QFont('Arial', 10))
-            self.label_weather.move(10, 500)
-            self.label_weather.show()
-            self.button.setText("Обновить данные")
-            self.button_clicked_count += 1
-            self._load_images("4.png")
-
+            self._get_data_weather()
+            self._load_images("4.png", 600, 600, 0, 250)
+            self._load_images("5.png", 250, 150, 330, 50)
+            self._load_images("3.png", 450, 350, 830, 10)
+            self._load_images("1.png", 450, 350, 0, -30)
         else:
             self.label_weather.deleteLater()
-            self.label_weather = QLabel(go_weather(), self)
-            self.label_weather.setFont(QFont('Arial', 10))
-            self.label_weather.move(10, 500)
-            self.label_weather.show()
-            self.button.setText("Обновить данные")
-            self._load_images("4.png")
+            self._get_data_weather()
 
     def _load_image_background(self, file_name: str) -> None:
         image = QPixmap(file_name)
-        label_image = QLabel(self)
-        label_image.setPixmap(image)
-        label_image.resize(image.width(), image.height())
-        print(image.width(), image.height())
+        image_background = QLabel(self)
+        image_background.setPixmap(image)
+        image_background.resize(image.width(), image.height())
 
-    def _load_images(self, file_name: str):
-        image1 = (QPixmap(file_name).scaled(600, 400, Qt.AspectRatioMode.KeepAspectRatio))
-        label_image1 = QLabel(self)
-        label_image1.setPixmap(image1)
-        label_image1.show()
-        label_image1.move(0, 0)
+    def _load_images(
+            self,
+            file_name: str,
+            width: int,
+            height: int,
+            ax: int,
+            ay: int
+    ) -> None:
+        image = (QPixmap(file_name)
+                 .scaled(width, height, Qt.AspectRatioMode.KeepAspectRatio))
+        loaded_image = QLabel(self)
+        loaded_image.setPixmap(image)
+        loaded_image.show()
+        loaded_image.move(ax, ay)
+
+    def _get_data_weather(self) -> None:
+        self.label_weather = QLabel(go_weather(), self)
+        self.label_weather.setFont(QFont('Cambria', 15))
+        self.label_weather.move(10, 500)
+        self.label_weather.show()
+        self.button.setText("Обновить данные")
+        self.button_clicked_count += 1
 
 def main():
     app = QApplication(sys.argv)
